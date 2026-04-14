776154531aeedf5890e9514e791457ce729c9973
Renuka <renukaagrawal97@gmail.com>
2026-04-14 11:17:05 +0530

    fixed folder structure

--- server.js
+++ server.js
@@ -12,11 +12,7 @@ connectDB();
 const app = express();
 
 // CORS configuration (MUST BE FIRST)
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 // Body parser
 app.use(express.json({ limit: '50mb' }));
